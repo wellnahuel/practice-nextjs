efa010db3bf151b68b44a066345c0cb21bf91a26
wellnahuel <nahuelcittadino@gmail.com>
2022-12-06 12:12:31 -0300

    layout, estilos basicos, menu

--- pages/store/index.js
+++ pages/store/index.js
@@ -1,1 +1,12 @@
 //cada vez que ponga en la ruta localhost3000/store se me carga el index.js de la carpeta store
+
+export default function Index() {
+    return (
+
+        <div>
+            <h1>
+                Store
+            </h1>
+        </div>
+    );
+}
